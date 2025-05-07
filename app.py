3e76f9b74192c36e4f7a997b9169685060836a05
wizztour <clxk14@gmail.com>
2025-05-07 22:31:02 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +39,5 @@ def get_festivals():
     return jsonify(festivals)
 
 if __name__ == "__main__":
-    app.run(host="0,0,0,0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
